3ef88927ff4b3821ed6b6fa7a640a2d49de3b24b
BlasphemyCoder <vascoalramos@outlook.pt>
2019-06-26 15:11:37 +0100

    Repo cleaned

--- SingleGrammar/TestVisitor.java
+++
@@ -1,7 +0,0 @@
-public class TestVisitor<T> extends GeneralBaseVisitor<T> {
-    @Override public T visitQuantity_decl(GeneralParser.Quantity_declContext ctx) {
-         return visitChildren(ctx); 
-        }
-   
-  }
-  
