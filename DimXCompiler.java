c2ec7633379202bc636f8365e67ed9e57a8135a9
HerouFenix <diogo0408@gmail.com>
2019-06-27 15:09:56 +0100

    incremneted dimx

--- DimXCompiler.java
+++ DimXCompiler.java
@@ -21,6 +21,8 @@ public class DimXCompiler extends GeneralBaseVisitor<ST> {
     }
 
     /* MAIN RULES */
+
+    //Main rule -> Starts module and adds all stats to the main function
     @Override
     public ST visitMain(GeneralParser.MainContext ctx) {
         assert (validTarget()); // Check if we have a valid stgFile
@@ -38,6 +40,9 @@ public class DimXCompiler extends GeneralBaseVisitor<ST> {
         return resultModule;
     }
 
+    /* PRINT RULES */
+
+    //Print rule -> Prints the expression
     @Override
     public ST visitPrint(GeneralParser.PrintContext ctx) {
         ST printResult = this.stg.getInstanceOf("print");
@@ -46,23 +51,60 @@ public class DimXCompiler extends GeneralBaseVisitor<ST> {
         return printResult;
     }
 
-    @Override public ST visitAssign(GeneralParser.AssignContext ctx) { 
-        ST assignResult = this.stg.getInstanceOf("assign");
+    /* ASSIGNMENT FUNCTIONS */
 
-        if(ctx.type != null){
-            visitDeclaration(ctx);
-        }
-                
+    //Assignment rule -> Assigns value to previously declared variable
+    @Override 
+    public ST visitJustAssign(GeneralParser.JustAssignContext ctx) {
+        ST assignResult = stg.getInstanceOf("assign");
+        String id = ctx.ID().getText();
+        Symbol symbol = SymbolTable.symbolTable.get(id);
+
+        assignResult.add("stat", visit(ctx.expr()).render());
+        assignResult.add("var",symbol.name());
+        assignResult.add("value",ctx.expr().varName);
+
+        return assignResult;
+    }
+
+    //Declare rule -> Declares new variable
+    @Override 
+    public ST visitDeclaration(GeneralParser.DeclarationContext ctx) {
+        ST decResult = stg.getInstanceOf("declare");
 
         String id = ctx.ID().getText();
-        return visitChildren(ctx); 
-    }
-    
-    //The delaration is only called if we have a type
-    public ST visitDeclaration(GeneralParser.AssignContext ctx) {
-        ST res = stg.getInstanceOf("stats");
+        Symbol symbol = SymbolTable.symbolTable.get(id);
+        
+        symbol.setVarName(newVar()); //Change var name to vX
 
-        String id = ctx.ID().getText();
+        decResult.add("type", symbol.type().name());
+        decResult.add("var",symbol.varName());
+
+        return decResult;
     }
+
+    //Declare and assign rule -> Declares new variable and assigns it a value
+    @Override 
+    public ST visitDeclareAndAssign(GeneralParser.DeclareAndAssignContext ctx) { 
+        ST result = stg.getInstanceOf("stats");
+
+        //Get the declaration
+        ST decResult = visit(ctx.declaration());
+
+        //Add value to declaration
+        decResult.add("value",ctx.expr().varName);
+        
+
+        //Write both declaration and assignment in seperate lines
+        result.add("stat",decResult.render());
+        result.add("stat",assResult.render());
+
+        return result;
+    }
+
+
+    /* EXPR RULES */
+
+
 
 }
